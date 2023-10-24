605dcd80cb5a69ab5571fdd3dc83b5906b5f9eee
JManK3900 <jmank09@gmail.com>
2023-10-24 16:28:55 -0600

    Environment completed

--- Environment.java
+++ Environment.java
@@ -22,7 +22,7 @@ public class Environment {
 	// Array of variable name(s)
 	//private String[] map = { "x" };
 	//Change to <String, Double> after changing all other file inputs to double(ALL FILES)
-	private HashMap<String, Integer> HashMap;
+	private HashMap<String, Double> HashMap;
 
 	/**
 	 * Adds or updates a variable in the environment.
